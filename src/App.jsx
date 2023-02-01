60f3e2b32d9cd738b3bd2ecffa5e9bec78999d9c
JunoUno507 <kspanim8@gmail.com>
2023-02-01 10:38:28 +0700

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,46 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function EmptyRow() {
   return (
-    <p>Hello World</p>
+    <>
+      <tr class='emptyrow'>
+        <td>x</td>
+        <td>x</td>
+        <td>x</td>
+        <td>x</td>
+        <td>x</td>
+      </tr>
+    </>
+  )
+}
+
+function Timetable() {
+  return (
+    <>
+      <table>
+        <tr>
+          <td>Mon</td>
+          <td>Tue</td>
+          <td>Wed</td>
+          <td>Thr</td>
+          <td>Fri</td>
+        </tr>
+        <EmptyRow />
+        <EmptyRow />
+        <EmptyRow />
+      </table>
+    </>
+  )
+}
+
+function App() {
+  // const [count, setCount] = useState(0)
+  return (
+    <>
+      <Timetable />
+      <p>Hello World!</p>
+    </>
   )
 }
 
